e27d4bc78f017165d3d8d005a26fa98e916daf4f
“adolfojb2” <adolfojb2@gmail.com>
2023-12-15 16:04:44 -0500

    Se crean 3 vistas con sus respectivas templates y el layout.html

--- MiPortafolio/urls.py
+++ MiPortafolio/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from portafolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name='index'),
+    path('proyectos/',views.proyectos, name='proyectos'),
+    path('contacto/', views.contacto, name='contacto')
 ]
---
+++ portafolio/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+     
+def proyectos(request):
+    return render(request, 'proyectos.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
